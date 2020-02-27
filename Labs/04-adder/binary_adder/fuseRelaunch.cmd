db0e87ee318d5d9ab258791d4587695d2b4232fb
lab661 <lab661@PC-445.urel.feec.vutbr.cz>
2020-02-27 13:42:40 +0100

    Added lab 04-adder

---
+++ Labs/04-adder/binary_adder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xsebor01/Digital-Electronics-1/Labs/04-adder/binary_adder/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/xsebor01/Digital-Electronics-1/Labs/04-adder/binary_adder/top_tb00_beh.prj" "work.top_tb00" 
