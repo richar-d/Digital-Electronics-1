b1baf3910dbd37f2c6ed621c9555181bd08ea0b9
richar-d <60732239+richar-d@users.noreply.github.com>
2020-03-13 16:37:50 +0100

    Add files via upload
    added lab 06

---
+++ Labs/06-display_driver/seven-segment-driver/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "C:/Users/Marek/Desktop/7-segment_driver2/7-segment_driver1/7-segment_driver/seven-segment-driver/top_tb00_isim_beh.exe" -prj "C:/Users/Marek/Desktop/7-segment_driver2/7-segment_driver1/7-segment_driver/seven-segment-driver/top_tb00_beh.prj" "work.top_tb00" 
